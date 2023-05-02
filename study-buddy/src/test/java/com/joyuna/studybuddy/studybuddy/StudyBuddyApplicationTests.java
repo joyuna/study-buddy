5d705b21b59d0ab0d86aa78b3aa0585f4a013669
joyounga <joayounga@gmail.com>
2023-05-02 23:59:44 +0900

    chore: Create project
    23.05.02

---
+++ study-buddy/src/test/java/com/joyuna/studybuddy/studybuddy/StudyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyuna.studybuddy.studybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
