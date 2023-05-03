d8ee52a8977db16bb934ad6ffb5b529ca663fbcb
joyounga <joayounga@gmail.com>
2023-05-03 11:54:44 +0900

    chore: Delete duplicate directory
    23.05.03

--- study-buddy/src/test/java/com/joyuna/studybuddy/StudyBuddyApplicationTests.java
+++ study-buddy/src/test/java/com/joyuna/studybuddy/StudyBuddyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.joyuna.studybuddy.studybuddy;
+package com.joyuna.studybuddy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StudyBuddyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
